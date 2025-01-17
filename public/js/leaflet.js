6cfc6c829664d5762ef977f602088b10e87509a5
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2025-01-17 17:56:54 +0100

    aufbau_grundstruktur

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -3,6 +3,11 @@ var map = L.map('map').setView([51.975, 7.61], 12);
 
 // add the base map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
- maxZoom: 19,
- attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Füge ein Event hinzu, damit die Karte sich anpasst, wenn das Fenster seine Größe ändert
+window.addEventListener('resize', function() {
+  map.invalidateSize(); // Veranlasst die Karte, ihre Größe neu zu berechnen
+});
